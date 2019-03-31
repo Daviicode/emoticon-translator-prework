1baf6382c5074e8bad1aac35690113064744f074
David zhang <d.ydzdavid@yahoo.com>
2019-03-31 21:11:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,28 @@
 # require modules here
+require "yaml"
 
-def load_library
-  # code goes here
-end
 
-def get_japanese_emoticon
+def load_library(path)
   # code goes here
+   file_path = {}
+   file = YAML.load_file(path)
+   get_meaning = {}
+   get_emoticon = {}
+   file.each do |key, value|
+     get_meaning[(value[1])] = key
+     get_emoticon[(value[0])] =value[1]
+   end
+   file_path["get_meaning"] = get_meaning
+   file_path["get_emoticon"] = get_emoticon
+   file_path
+ end
+   
+   
+
+def get_japanese_emoticon(path, emoticon)
+  # code goes here
+  file = load_library(path)
+  if file
 end
 
 def get_english_meaning
