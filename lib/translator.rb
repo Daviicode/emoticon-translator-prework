b933cf604922f05ecd75bb3e60aa6c973667c0bf
David zhang <d.ydzdavid@yahoo.com>
2019-03-31 21:34:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,7 +25,7 @@ def get_japanese_emoticon(path, emoticon)
    if file["get_emoticon"].keys.include?(emoticon)
      return file["get_emoticon"][emoticon]
    else
-     "Sorry not a known emoticon."
+     "Sorry, not a known emoticon."
    end
 end
 
